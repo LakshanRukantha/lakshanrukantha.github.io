abad6ac57a697ec4eb88e4a2138e6729a7b54027
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2022-05-22 20:39:45 +0530

    Navigation panel auto expand on load error fixed

--- MyProjects/Portfolio-Website-01/js/main.js
+++ MyProjects/Portfolio-Website-01/js/main.js
@@ -3,23 +3,32 @@ const scrollUp = document.querySelector(".to-top");
 window.addEventListener("scroll", () => {
   if (window.pageYOffset > 1000) {
     scrollUp.classList.add("active-top");
-  }else{
-      scrollUp.classList.remove("active-top");
+  } else {
+    scrollUp.classList.remove("active-top");
   }
 });
 
 var options = {
-  strings: ['Front-End Developer', 'UI/UX Designer', 'Sound Designer', 'Photographer'],
+  strings: [
+    "Front-End Developer",
+    "UI/UX Designer",
+    "Sound Designer",
+    "Photographer",
+  ],
   typeSpeed: 50,
   loop: true,
   backSpeed: 30,
 };
 
-var typed = new Typed('.typing-text', options);
+var typed = new Typed(".typing-text", options);
 
-const navLinks = document.querySelectorAll('.nav-card-item')
-const menuToggle = document.getElementById('navbarNav')
-const bsCollapse = new bootstrap.Collapse(menuToggle)
-navLinks.forEach((l) => {
-    l.addEventListener('click', () => { bsCollapse.toggle() })
-})
+function navClick() {
+  const navLinks = document.querySelectorAll(".nav-card-item");
+  const menuToggle = document.getElementById("navbarNav");
+  const bsCollapse = new bootstrap.Collapse(menuToggle);
+  navLinks.forEach((l) => {
+    l.addEventListener("click", () => {
+      bsCollapse.toggle();
+    });
+  });
+}
