dfed794d2ea29a7892150e8803cd72e048881570
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2022-05-25 02:08:37 +0530

    Added tilt.js to the project.

--- MyProjects/Portfolio-Website-01/js/main.js
+++ MyProjects/Portfolio-Website-01/js/main.js
@@ -25,3 +25,22 @@ var typed = new Typed(".typing-text", options);
 $(".js-scroll-trigger").click(function () {
   $(".navbar-collapse").collapse("hide");
 });
+VanillaTilt.init(document.querySelector(".img"), {
+  max: 10,
+  speed: 400,
+  easing: "cubic-bezier(.03,.98,.52,.99)",
+  "max-glare": 0.4,
+  startX: 0,
+  startY: 0,
+  glare: true,
+});
+
+VanillaTilt.init(document.querySelector(".bio-img"), {
+  max: 10,
+  speed: 400,
+  easing: "cubic-bezier(.03,.98,.52,.99)",
+  "max-glare": 0.4,
+  startX: 0,
+  startY: 0,
+  glare: true,
+});
